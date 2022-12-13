16096112977837437ff8265edb7e9b94a8714f10
ellie <ellie.zacharelou@gmail.com>
2022-12-13 18:22:57 +0200

    first commit

---
+++ test_script_1.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service_obj = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+driver.maximize_window()
+driver.get("http://qatest.schoox.com/login")
+
+# LOGIN
+driver.find_element(By.CSS_SELECTOR, "div input[type='email']").send_keys("admin@schoox.com")
+driver.find_element(By.CSS_SELECTOR, "div input[type='password']").send_keys("123456")
+driver.find_element(By.CSS_SELECTOR, "button").click()
+
+# GO TO TRAINING
+driver.find_element(By.CSS_SELECTOR, "a[href='/training']").click()
+
+# CHOOSE QA CATEGORY
+driver.find_element(By.XPATH, "//div[@class='category_item'][1]").click()
+
+# VALIDATE LISTS CONTENTS
+items = driver.find_elements(By.XPATH, "//tr")
+courses = []
+for item in items:
+    courses.append(item.find_element(By.XPATH, "td/a").text)
+assert courses == ["QA course", "ΒΑ course", "Μάθημα για τους Devs", "Μάθημα για automation"]
+
---
+++ test_script_2.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service_obj = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+driver.maximize_window()
+driver.get("http://qatest.schoox.com/login")
+
+# LOGIN
+driver.find_element(By.CSS_SELECTOR, "div input[type='email']").send_keys("admin@schoox.com")
+driver.find_element(By.CSS_SELECTOR, "div input[type='password']").send_keys("123456")
+driver.find_element(By.CSS_SELECTOR, "button").click()
+
+# GO TO https://qatest.schoox.com/6/steps
+driver.find_element(By.CSS_SELECTOR, "a[href='/training']").click()
+driver.find_element(By.CSS_SELECTOR, "a[href='/6/steps']").click()
+
+# ENROLL TO THE COURSE
+driver.find_element(By.CSS_SELECTOR, ".enroll").click()
+
+# COMPLETE ALL THE STEPS
+step_list = ["(//b[text()='Complete'])[1]", "(//b[text()='Complete'])[2]", "(//b[text()='Complete'])[3]", "(//b[text()='Complete'])[4]"]
+for step in step_list:
+    driver.find_element(By.XPATH, step).click()
+    alert = driver.switch_to.alert
+    alert.accept()
+
+# VALIDATE PROGRESS IS 100%
+progress = driver.find_element(By.XPATH, "//div[@class='top_course_progress']").text
+assert progress == 'Course Progress: 100.00%'
+
